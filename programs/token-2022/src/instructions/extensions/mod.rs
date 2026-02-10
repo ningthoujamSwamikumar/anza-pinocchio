eb07dea85efe0a875538140fe36dfc27f6c1a359
Burhan khaja <118989617+burhankhaja@users.noreply.github.com>
2026-02-10 16:06:16 +0530

    Token2022: Add PermanentDelegate extension instruction builders  (#275)
    * token-2022: Add permanent delegate extension
    
    Co-authored-by: M. Daeva <embedmad@gmail.com>
    
    * Nits
    
    * token-2022: Rebase changes for `PermanentDelegate` (#56)
    
    * Rebase
    
    * Cosmetics
    
    ---------
    
    Co-authored-by: M. Daeva <embedmad@gmail.com>
    Co-authored-by: febo <febo@anza.xyz>

--- programs/token-2022/src/instructions/extensions/mod.rs
+++ programs/token-2022/src/instructions/extensions/mod.rs
@@ -1,6 +1,7 @@
 pub mod confidential_mint_burn;
 pub mod default_account_state;
 pub mod memo_transfer;
+pub mod permanent_delegate;
 pub mod transfer_hook;
 
 #[repr(u8)]
@@ -8,6 +9,7 @@ pub mod transfer_hook;
 pub enum ExtensionDiscriminator {
     DefaultAccountState = 28,
     MemoTransfer = 30,
+    PermanentDelegate = 35,
     TransferHook = 36,
     ConfidentialMintBurn = 42,
 }
